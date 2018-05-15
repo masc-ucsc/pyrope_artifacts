5dd3dee567793c3af988224b4f4ba8cced0edf5a
Jose Renau <renau@ucsc.edu>
2018-05-15 14:42:54 -0700

    clang format

--- source/utils/Exception.hpp
+++ source/utils/Exception.hpp
@@ -1,40 +1,45 @@
 #ifndef PYROPE_EXCEPTION_H_
 #define PYROPE_EXCEPTION_H_
 
-#include <string>
 #include <stdexcept>
+#include <string>
 
-namespace Pyrope
-{
-  class Exception : public std::runtime_error
-  {
-    public:
-      Exception(const std::string &m) : std::runtime_error(msg) { }
+namespace Pyrope {
+class Exception : public std::runtime_error {
+public:
+  Exception(const std::string &m)
+      : std::runtime_error(msg) {
+  }
 
-    protected:
-      std::string msg;
-  };
+protected:
+  std::string msg;
+};
 
-  class TypeError : public Exception
-  {
-    public:
-      TypeError(const std::string &msg) : Exception(msg) { }
-  };
+class TypeError : public Exception {
+public:
+  TypeError(const std::string &msg)
+      : Exception(msg) {
+  }
+};
 
-  const std::string DEBUG_MSG = "Not implemented yet";
+const std::string DEBUG_MSG = "Not implemented yet";
 
-  class DebugError : public Exception
-  {
-    public:
-      DebugError() : Exception(DEBUG_MSG) { }
-      DebugError(const std::string &msg) : Exception(DEBUG_MSG + ": " + msg) { }
-  };
+class DebugError : public Exception {
+public:
+  DebugError()
+      : Exception(DEBUG_MSG) {
+  }
+  DebugError(const std::string &msg)
+      : Exception(DEBUG_MSG + ": " + msg) {
+  }
+};
 
-  class LogicError : public Exception
-  {
-    public:
-      LogicError(const std::string &msg) : Exception(msg) { }
-  };
-}
+class LogicError : public Exception {
+public:
+  LogicError(const std::string &msg)
+      : Exception(msg) {
+  }
+};
+} // namespace Pyrope
 
 #endif
